e51145a0448b90f77bbcb7ff19ec28a8d98d1bde
Milene Taborda <milenetaborda1@outlook.com>
2020-04-08 18:36:24 -0300

    Revolvido problema com método Post

--- future-ninjas/src/components/QueroContratar/QueroContratar.js
+++ future-ninjas/src/components/QueroContratar/QueroContratar.js
@@ -6,139 +6,137 @@ class QueroContratar extends React.Component {
         super(props)
         this.state = {
 
-            inputTitulo:'',
-            inputCidade:'',
-            inputDescricao:'',
-            inputTecnologia:'',
-            inputValor:'',
-            inputDueDate:'',
-            inputMetodoDePagamento:''
-
+         valorInputTitulo: '',
+         valorInputValor: '',
+         valorInputDescricao: '',
+         valorSelectPagamento: '',
+         valorInputDataVencimento: '',
+         valorInputCidade: '',
+         valorInputTecnologia: ''
         }
     }
-    onchangeTitulo = e => {
-        this.setState({inputTitulo:e.target.value})
 
-    }
     onchangeCidade = e => {
-        this.setState({inputCidade:e.target.value})
-
+         this.setState({ valorInputCidade: e.target.value })
     }
+
+    onchangeTitulo = e => {
+        this.setState({ valorInputTitulo: e.target.value })
+    }
+
     onchangeDescricao = e => {
-        this.setState({inputDescricao:e.target.value})
-
+        this.setState({ valorInputDescricao: e.target.value })
     }
-    onchangeTecnolgia = e => {
-        this.setState({inputTecnologia:e.target.value})
 
-    }
     onchangeValor = e => {
-        this.setState({inputValor:e.target.value})
-
+        this.setState({ valorInputValor: e.target.value })
     }
-    onchangeDueDate = e => {
-        this.setState({inputDueDate:e.target.value})
 
-    }
     onchageMetodoDePagamento = e => {
-        this.setState({inputMetodoDePagamento:e.target.value})
+        this.setState({ valorSelectPagamento: e.target.value })
+    }
 
+
+    onchageVencimento = e => {
+        this.setState({ valorInputDataVencimento: e.target.value })
     }
     
-    onclickVaga = () => {
-        const body = {
+    onchageTecnologia = e => {
+        this.setState({ valorInputTecnologia: e.target.value })
+    }
 
-            title :this.state.inputTitulo,
-            cidade:this.state.inputCidade,
-            description:this.state.inputDescricao,
-            value:this.state.inputValor,
-            dueDate:this.state.inputDueDate,
-            tags:this.state.inputTecnologia,
-            paymentMethods:this.state.inputMetodoDePagamento
-        }
-        axios.post(`https://us-central1-future-apis.cloudfunctions.net/futureNinjas/jobs`, body,
-        {
-            headers:{
+    criarNovaVaga = (e)=> {
+        e.preventDefault();
 
-                "api-token":"futureninjas",
-                'Content-Type': 'application/json'
-             }
-
-        }) .then((response) => {
-            window.alert("Vaga criada");
-            console.log(response)
-        })
-        .catch(() => {
-            console.log("Errorrrrrr")
-        })
+    const body = {
         
+            "title": this.state.valorInputTitulo,
+            "description": this.state.valorInputDescricao,
+            "value": this.state.valorInputValor,
+            "paymentMethods": [this.state.valorSelectPagamento],
+            "dueDate": this.state.valorInputDataVencimento,
+            "cidade": this.state.valorInputCidade,
+            'tecnologia': [this.state.valorInputTecnologia]
+    }
+
+    axios.post('https://us-central1-future-apis.cloudfunctions.net/futureNinjas/jobs', body, {
+        headers: {
+            'api-token': 'projetoninja'
+        }
+    }).then(response => {
+        alert('Deeeeu!')
+        console.log(response.data)
+    }).catch(() => {
+        alert('Errrrouuu')
+    })
     }
 
     render() {
         return (
             <div>
                 <h1>Teste</h1>
-                <form>
+                <div>
                     <label htmlFor= "título">Título: </label>
                     <input
-                        value= {this.state.inputTitulo}
+                        value= {this.state.valorInputTitulo}
                         onChange = {this.onchangeTitulo}
                         name ="título"
 
                     />
                     <br/>
+
                     <label htmlFor= "cidade">Cidade: </label>
                     <input
-                        value= {this.state.inputCidade}
+                        value= {this.state.valorInputCidade}
                         onChange = {this.onchangeCidade}
                         name ="cidade"
-
                     />
-                    <br/>
+                    <br/> 
 
-                    {/* <label>Método de pagamento: </label>
-                    <select value={this.state.inputMetodoDePagamento} onChange={this.onchageMetodoDePagamento}>
+                    <label>Método de pagamento: </label>
+                    <select value={this.state.valorSelectPagamento} onChange={this.onchageMetodoDePagamento}>
                         <option>Cartão de crédito</option>
                         <option>BitCoin</option>
                         <option>Dinheiro</option>
-                    </select> */}
+                    </select>
                     <br/>
 
-                    {/* <label htmlFor= "vencimento">Data de vencimento: </label>
+                    <label htmlFor= "vencimento">Data de vencimento: </label>
                     <input
-                        value= {this.state.inputDueDate}
-                        onChange = {this.onchangeDueDate}
+                        value= {this.state.valorInputDataVencimento}
+                        onChange = {this.onchageVencimento}
                         name ="vencimeto"
-
-                    /> */}
+                        
+                    />
                     <br/>
                     <label htmlFor= "valor">Valor: </label>
                     <input
-                        value= {this.state.inputValor}
+                        value= {this.state.valorInputValor}
                         onChange = {this.onchangeValor}
                         name ="valor"
-
-                    />
+                        
+                        />
                     <br/>
                     <label htmlFor= "descricao">Descrição: </label>
                     <textarea
-
-                        value ={this.state.inputDescricao}
+                        value ={this.state.valorInputDescricao}
                         onChange={this.onchangeDescricao}
                         name ="descricao"
-
+                        
                     >
                     </textarea>
                     <br/>
-                    {/* <label htmlFor= "tecnologias">Tecnologias: </label>
+                     <label htmlFor= "tecnologia">Tecnologias: </label>
                     <input 
-                        value= {this.state.inputTecnologia}
-                        onChange = {this.onchangeTecnolgia}
+                        value= {this.state.valorInputTecnologia}
+                        onChange = {this.onchageTecnologia}
                         name ="tecnologia"
-                        
-                    /> */}
-                    <button onClick={this.onclickVaga}>Anunciar vaga</button>
-                </form>
+                    />  
+                   
+             
+
+                    <button onClick={this.criarNovaVaga}>Anunciar vaga</button>
+                </div>
             </div>
         )
     }
